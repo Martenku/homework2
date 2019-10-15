9bea117e45961273483b8ca55dce0882a718977f
Marten Kuusmann <44394684+Martenku@users.noreply.github.com>
2019-10-15 12:49:03 +0300

    Update index.html
    Valmis ülesanded

--- Course.js
+++
@@ -1,5 +0,0 @@
-function(title, semester, grade){
-	this.title = title;
-	this.semester = semester;
-	this.grade = grade;
-}
--- User.js
+++
@@ -1,7 +0,0 @@
-function(firstname, lastname, birthdate, faculty, gpa){
-	this.firstname = firstname;
-	this.lastname = lastname;
-	this.birthdate = birthdate;
-	this.faculty = faculty;
-	this.gpa = gpa;
-}
